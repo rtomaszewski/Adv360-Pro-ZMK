b853e8c00c3e0be94e23a13d9e208c1f20a145d9
rado <radoslaw.tomaszeski@gmail.com>
2023-05-23 11:07:06 +0100

    added macro to tape '.*' sequence

--- copy-utf-to-win.sh
+++ copy-utf-to-win.sh
@@ -3,4 +3,14 @@
 windir="/mnt/c/Users/rados/Downloads/_zmk"
 lindir="/home/rado/Adv360-Pro-ZMK/firmware"
 
-mv -v $lindir/*.uf2 $windir/ 
+while true; do
+  echo -n "."; sleep 1s
+
+  if [ $(ls $lindir/*.uf2 2>/dev/null | wc -l) -ne 0 ] ; then 
+    echo ,; sleep 1s; #to make sure that the files are actually writen and closed; 
+                      # we wouldn't like to copy corrupted/truncated files
+    echo; echo $(date)
+    mv -v $lindir/*.uf2 $windir/ 
+    echo ; echo
+  fi
+done
